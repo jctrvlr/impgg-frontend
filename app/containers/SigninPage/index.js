0abef471b77dfbc45251ae40fab33533dade45ac
Jack Cummings <jack@jackc.io>
2019-07-06 00:58:21 -0400

    Added new pages and theme.js

---
+++ app/containers/SigninPage/index.js
@@ -0,0 +1,172 @@
+/**
+ *
+ * SigninPage
+ *
+ */
+
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+// import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+import makeSelectSigninPage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+// import messages from './messages';
+
+function MadeWithLove() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Built with love by the '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Material-UI
+      </Link>
+      {' team.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export function SigninPage() {
+  useInjectReducer({ key: 'signinPage', reducer });
+  useInjectSaga({ key: 'signinPage', saga });
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Helmet>
+        <title>ImpGG - Login</title>
+        <meta
+          name="description"
+          content="Log in to ImpGG -- Your mischievously short link shortener."
+        />
+      </Helmet>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Log In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link
+                  component={RouterLink}
+                  to="/passwordrecovery"
+                  variant="body2"
+                >
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link component={RouterLink} to="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={5}>
+          <MadeWithLove />
+        </Box>
+      </Container>
+    </div>
+  );
+}
+
+SigninPage.propTypes = {
+  // dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  signinPage: makeSelectSigninPage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(withConnect)(SigninPage);
---
+++ app/theme.js
@@ -0,0 +1,31 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+
+const themeDark = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: red,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
+const themeLight = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: red,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
+// eslint-disable-next-line no-unused-vars
+const theme = mode => (mode === 'dark' ? themeDark : themeLight);
+
+export default themeLight;
