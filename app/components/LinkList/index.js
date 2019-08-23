773373fefac67a55782af27beb9bc0e0e0434c25
Jack Cummings <jack@jackc.io>
2019-08-23 00:06:55 -0400

    Updated styles for hero content and form and link list for mobile

--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -36,6 +36,9 @@ const useStyles = makeStyles(theme => ({
     overflow: 'hidden',
     textOverflow: 'ellipsis',
     maxWidth: '400px',
+    [theme.breakpoints.up(768)]: {
+      marginLeft: '56px',
+    },
   },
   a: {
     color: 'transparent',
@@ -58,11 +61,7 @@ function LinkList({ uriHistory }) {
       return (
         <React.Fragment key={shortid.generate()}>
           <ListItem alignItems="flex-start" className={classes.li}>
-            <ListItemText
-              className={classes.lItemText}
-              inset
-              primary={item.url}
-            />
+            <ListItemText className={classes.lItemText} primary={item.url} />
             <ListItemText
               className={classes.lItemText}
               primary={
@@ -92,11 +91,7 @@ function LinkList({ uriHistory }) {
     return (
       <React.Fragment key={shortid.generate()}>
         <ListItem alignItems="flex-start" className={classes.li}>
-          <ListItemText
-            className={classes.lItemText}
-            inset
-            primary={item.url}
-          />
+          <ListItemText className={classes.lItemText} primary={item.url} />
           <ListItemText
             className={classes.lItemText}
             primary={
@@ -113,7 +108,7 @@ function LinkList({ uriHistory }) {
             }
           />
           <ListItemIcon>
-            <CopyToClipboard text={item.shortLink}>
+            <CopyToClipboard text={baseUrl + item.shortLink}>
               <IconButton aria-label="copy">
                 <FileCopy />
               </IconButton>
