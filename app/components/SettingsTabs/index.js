947fd8971d786d0b06fb9a7916909e31fc414e36
John Cummings <jcummings5@gmail.com>
2020-04-01 17:30:53 -0700

    Changed settingstabs to formatted messages for translations.

--- app/components/SettingsTabs/index.js
+++ app/components/SettingsTabs/index.js
@@ -13,8 +13,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
-// import { FormattedMessage } from 'react-intl';
-// import messages from './messages';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
 
 function LinkTab(props) {
   return <Tab component={RouterLink} {...props} />;
@@ -24,9 +24,18 @@ function SettingsTabs({ tabValue }) {
   return (
     <AppBar position="static" component="div">
       <Tabs variant="fullWidth" aria-label="nav tabs example" value={tabValue}>
-        <LinkTab label="Profile" to="/settings/profile" />
-        <LinkTab label="Security and Privacy" to="/settings/security" />
-        <LinkTab label="Subscription" to="/settings/billing" />
+        <LinkTab
+          label={<FormattedMessage {...messages.profile} />}
+          to="/settings/profile"
+        />
+        <LinkTab
+          label={<FormattedMessage {...messages.security} />}
+          to="/settings/security"
+        />
+        <LinkTab
+          label={<FormattedMessage {...messages.subscription} />}
+          to="/settings/billing"
+        />
       </Tabs>
     </AppBar>
   );
--- app/components/SettingsTabs/messages.js
+++ app/components/SettingsTabs/messages.js
@@ -9,8 +9,16 @@ import { defineMessages } from 'react-intl';
 export const scope = 'app.components.SettingsTabs';
 
 export default defineMessages({
-  header: {
-    id: `${scope}.header`,
-    defaultMessage: 'This is the SettingsTabs component!',
+  profile: {
+    id: `${scope}.profile`,
+    defaultMessage: 'Profile',
+  },
+  security: {
+    id: `${scope}.security`,
+    defaultMessage: 'Security & Privacy',
+  },
+  subscription: {
+    id: `${scope}.subscription`,
+    defaultMessage: 'Subscription',
   },
 });
