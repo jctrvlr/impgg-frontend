8d47840efae9da5d0e00f7d0800cf9bd9bf39dde
John Cummings <jcummings5@gmail.com>
2020-04-04 23:37:52 -0700

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /Users/johncummings/projects/impgg/impgg-frontend
+cd /Users/batman/projects/impgg/impgg-frontend
 yarn build
-rsync -vrzc --delete /Users/johncummings/projects/impgg/impgg-frontend/build/ impggprodfrontdeploy:/var/www/html/
+rsync -vrzc --delete /Users/batman/projects/impgg/impgg-frontend/build/ impggprodfrontdeploy:/var/www/html/
